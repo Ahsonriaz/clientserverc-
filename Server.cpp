493b14d3e963db201012eae85e711af663f29de7
Ahsonriaz <80016851+Ahsonriaz@users.noreply.github.com>
2021-08-04 00:21:06 +0500

    Add files via upload

--- Server.cpp
+++ Server.cpp
@@ -6,130 +6,168 @@
 #include <string.h>
 #include <arpa/inet.h>
 #include <string>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
 
 using namespace std;
 
+void dopro(int sock);
 
-int main() {
+int server_func()
+{
 
-    //create socket
-    int listening= socket(AF_INET,SOCK_STREAM,0);
+    int listening = socket(AF_INET, SOCK_STREAM, 0);
 
-    //3 onwaord outup if success, automatic increment, lsof
-
-    if (listening<=0){
-        cout<<"port not opened"<<endl;
+    if (listening <= 0)
+    {
+        cout << "port not opened" << endl;
         return -1;
     }
 
-    //bind the socket (picking random IP) (port=3456)
     sockaddr_in hint;
-    hint.sin_family=AF_INET;
-    //Convert little endian to big endian HOT to NOT
-    hint.sin_port=htons(54000); //ntohs!
-    //run on any IP
+    hint.sin_family = AF_INET;
+
+    hint.sin_port = htons(54000);
+
     inet_pton(AF_INET, "0.0.0.0", &hint.sin_addr);
 
-    //bind now
-    if (bind(listening, (sockaddr*)&hint, sizeof(hint))==-1) {
+    if (bind(listening, (sockaddr *)&hint, sizeof(hint)) == -1)
+    {
 
-        cout<< "cannot bind to  IP/PORT";
+        cout << "cannot bind to  IP/PORT";
         return -2;
-
     }
 
-    //mark socket for listening
-    //queue size generate, how many pending connections, timeout, max queue size, 
-    //if zero immediate disconnect
-    if (listen(listening, 10)==-1) {
+    if (listen(listening, 10) == -1)
+    {
 
-        cout<<"can't listen";
+        cout << "can't listen";
         return -3;
-
     }
 
-    //accept a call 
-    //wiats in clocking state, make new thread, 
     sockaddr_in client;
-    socklen_t clientsize=sizeof(client);
+    socklen_t clientsize = sizeof(client);
     char host[NI_MAXHOST];
-    char svc [NI_MAXSERV];
+    char svc[NI_MAXSERV];
 
-    int clientsocket=accept(listening, (sockaddr*)&client, &clientsize);
+    listen(listening, 10);
 
-    if (clientsocket == -1) {
-
-        cout<<"problem with client connecting";
-        return -4;
-    }
-    //must take multiple connections
-    //must not close 
-    close(listening);
-
-    //clean garbage
-
-    memset(host, 0, NI_MAXHOST);
-    memset(svc,0,NI_MAXSERV);
-
-    int result=getnameinfo((sockaddr*)&client,sizeof(client), host, NI_MAXHOST, svc, NI_MAXSERV, 0);
-
-    if (result) {
-
-        cout<<"host connected on"<<svc <<endl;
-
-    }
-
-    else {
-
-        inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
-        cout<<host<< "connected on "<<ntohs(client.sin_port)<<endl;
-
-    }
-
-    //display message
     char buf[4096];
+    int bytes;
     string userinput;
 
+    while (true)
+    {
+
+        int clientsocket = accept(listening, (sockaddr *)&client, &clientsize);
+
+        if (clientsocket == -1)
+        {
+
+            cout << "problem with client connecting";
+            return -4;
+        }
+
+        int pid = fork();
+
+        // memset(host, 0, NI_MAXHOST);
+        // memset(svc, 0, NI_MAXSERV);
+
+        // int result = getnameinfo((sockaddr *)&client, sizeof(client), host, NI_MAXHOST, svc, NI_MAXSERV, 0);
+
+        // if (result)
+        // {
+
+        //     cout << "host connected on" << svc << endl;
+        // }
+
+        // else
+        // {
+
+        //     inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
+        //     cout << host << "connected on " << ntohs(client.sin_port) << endl;
+        // }
+
+        if (pid < 0)
+        {
+
+            cout << "error on thread creation";
+        }
+
+        else if (pid == 0)
+        {
+            close(listening);
+            dopro(clientsocket);
+        }
+
+        else
+        {
+            close(clientsocket);
+        }
+    }
+
+    return 0;
+}
+
+void dopro(int sock)
+{
+    char buf[4096];
     int bytes;
-    while(true) {
+    string userinput;
+
+    while (true)
+    {
 
         //clear buffer
         memset(buf, 0, 4096);
 
         //wait for a message
-        bytes= recv(clientsocket, buf, 4096, 0);
-        if (bytes < 0) {
-            cout<< "there was a connection issue"<<endl;
-            break;
-        } else if (bytes==0){
-            cout<<"The client disconnected"<< endl;
+        int bytes = recv(sock, buf, 4096, 0);
+        if (bytes < 0)
+        {
+
+            cout << "there was a connection issue" << endl;
             break;
         }
-        
+
+        else if (bytes == 0)
+        {
+
+            cout << "The client disconnected" << endl;
+            break;
+        }
+
         //display
-        cout<<"\033[1;32m"<< "Recieved: " <<"\033[1;31m"<< buf <<endl;
+        cout << "\033[1;32m"
+             << "Recieved: "
+             << "\033[1;31m" << string(buf, 0, bytes) << endl;
 
-//	bytes = send(clientsocket, buf, bytes, 0 );
+        cout << "Server ";
+        getline(cin, userinput);
 
-      //enter text get entire line
-      cout<<" >: ";
-      getline(cin, userinput);
-
-      //send to server
-      bytes = send(clientsocket, userinput.c_str(), userinput.size()+1, 0);
-
-	
-
+        bytes = send(sock, userinput.c_str(), userinput.size() + 1, 0);
     }
-
-    //close socket
-    close(clientsocket);
-
-    return 0;
-
+    
 }
 
-//write client : 
+int main()
+{
+
+    cout << "SERVER CONNECTED" << endl;
+    server_func();
+}
+
+
+// //display message
+    
+
+
+
+
+
+
+//write client :
 //new client thread only for that client
 //one main thread only for accept - blocking
 //asynchronus comm with timeout, all clients single thread, file descriptor non blocking
@@ -138,12 +176,10 @@ int main() {
 
 //when client close socket, disconnect thread/kill
 
-
 // TCP/IP->connection oriented, needs an assigned endpoints
 
 // Server
 // socket()-> bind()->listen ()->accept() -> read() /write()->close
-
 
 // client
 // socket()->connect()->read/write->close
